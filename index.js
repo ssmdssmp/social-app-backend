42fb09c07dd3834a0745d419eca8f0c037200ed5
ssmdssmp <dmitryy.volov@gmail.com>
2022-12-15 18:11:43 +0200

    try 3

--- index.js
+++ index.js
@@ -22,16 +22,7 @@ mongoose.connect(process.env.MONGO_URL);
 
 const jsonParser = bodyParser.json();
 mongoose.set("strictQuery", true);
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 //socket for online users
 
 const http = require("http").createServer(app);
